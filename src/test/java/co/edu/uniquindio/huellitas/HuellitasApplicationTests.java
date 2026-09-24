90850883377222629c1daf91fb1428e27647bb7b
StefaniaH016 <160367091+StefaniaH016@users.noreply.github.com>
2026-09-23 19:09:33 -0500

    Repositorio Inicializado

---
+++ src/test/java/co/edu/uniquindio/huellitas/HuellitasApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.huellitas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuellitasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
